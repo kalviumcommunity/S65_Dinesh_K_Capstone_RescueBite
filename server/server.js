55428831548846e6ccf8da7f943fbc7dd0f6a4d4
dinesh <shrutidinesh1124@gmail.com>
2025-03-23 15:26:33 +0530

    get request is successfull

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const userRoutes = require('./routes/user-routes')
 const app = express();
 
 app.use(express.json());
@@ -6,6 +7,8 @@ app.use(express.json());
 app.get('/', function (req,res) {
     res.send('Server is running');
 })
+
+app.use('/', userRoutes)
 
 const PORT = 3000
 
